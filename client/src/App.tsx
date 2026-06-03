7fec4339a19d36835cf3465c60b8a840e15388f1
diporadewale <ola@tilzmart.com>
2026-06-03 23:00:53 +0100

    Add skip-to-content link and main landmark id (a11y)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -34,8 +34,16 @@ function ScrollToTop() {
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-white">
+      <a
+        href="#main-content"
+        className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:rounded-md focus:bg-[#1E40AF] focus:px-4 focus:py-2 focus:text-white"
+      >
+        Skip to content
+      </a>
       <Navigation />
-      <main className="flex-1">{children}</main>
+      <main id="main-content" tabIndex={-1} className="flex-1">
+        {children}
+      </main>
       <Footer />
     </div>
   );
